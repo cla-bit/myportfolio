368c7f6df09f973e7cc4453eead5263ae66fb300
petermbachu.bincom <petermbachu.bincom@gmail.com>
2024-04-09 18:28:44 +0100

    updated

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView, ListView, FormView
 
 from .forms import ContactForm
@@ -30,6 +32,10 @@ class ProjectView(ListView):
     context_object_name = 'projects'
     paginate_by = 3
 
+    @method_decorator(cache_page(60 * 15))  # cache for 15 minutes
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = "All Projects"
@@ -46,3 +52,8 @@ class ContactView(FormView):
         send_contact_email(contact)
         messages.success(self.request, 'Your message has been sent. Thank you!')
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Connect with Me!"
+        return context
